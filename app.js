99560ca1910198a4b6f4d38554f2a0ffa2918b0a
Tetiana Tsar <protetsar@gmail.com>
2023-11-22 02:05:17 +0200

    add cors

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const cors = require('cors')
+
 const bodyParser = require("body-parser");
 const { checkAuth } = require('./middleware');
 const { login, register, getAllPlaces, getPlaceById, createPlace, updatedPlace, deletePlace } = require('./controllers');
 
 const app = express();
-    
+
+app.use(cors())
+
 app.use(bodyParser.json());
 
 
@@ -12,7 +16,7 @@ app.post("/login", login);
 
 app.post("/register", register);
 
-app.get("/places", checkAuth, getAllPlaces);
+app.get("/places",  getAllPlaces);
 
 app.get("/places/:id", checkAuth, getPlaceById);
 
